74096a84c342ea4fbadb5530f55af923caa346f1
Tola Fasoyiro <tfasoyiro@salmon.com>
2016-05-15 00:58:28 +0100

    Audi_Ecom_Features_150516

---
+++ src/test/java/com/salmon/test/step_definitions/gui/audi/AudiEcomProductPageSteps.java
@@ -0,0 +1,11 @@
+package com.salmon.test.step_definitions.gui.audi;
+
+/**
+ * Created by tfasoyiro on 15/05/2016.
+ */
+public class AudiEcomProductPageSteps {
+
+
+    //PDP, PLP, Search, Navigation, Compare, Wishlist
+
+}
